de63eb11f6af056bb17ecc51e02669c2dbd3feba
hsharma1331 <h4s143@gmail.com>
2024-01-29 16:59:02 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
 require ('dotenv').config();
+const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoDB = require('./db')
